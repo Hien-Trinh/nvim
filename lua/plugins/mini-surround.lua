2ed7fa3fc91ad47d107eacdc3d8057d7f3735579
Hien-Trinh <hientrinhminh32@gmail.com>
2024-07-22 14:28:47 +0700

    added mini-surround to normal and visual mode

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -6,11 +6,11 @@ return {
     local opts = LazyVim.opts("mini.surround")
     local mappings = {
       { opts.mappings.add, desc = "Add Surrounding", mode = { "n", "v" } },
-      { opts.mappings.delete, desc = "Delete Surrounding" },
+      { opts.mappings.delete, desc = "Delete Surrounding", mode = { "n", "v" } },
       { opts.mappings.find, desc = "Find Right Surrounding" },
       { opts.mappings.find_left, desc = "Find Left Surrounding" },
       { opts.mappings.highlight, desc = "Highlight Surrounding" },
-      { opts.mappings.replace, desc = "Replace Surrounding" },
+      { opts.mappings.replace, desc = "Replace Surrounding", mode = { "n", "v" } },
       { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
     }
     mappings = vim.tbl_filter(function(m)
